5dfd14c5ba47b59a3f27884a334752b9c6aa319e
mvhmz81 <mvhmz@yahoo.co.uk>
2023-01-25 14:40:02 +0000

    hello

--- components/QuestionnaireForm/QuestionnaireForm.js
+++ components/QuestionnaireForm/QuestionnaireForm.js
@@ -1,17 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { useScore } from "@/context/useScore";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-export default function QuestionnaireForm({ questionsArray, questionStep }) {
+export default function QuestionnaireForm({
+  questionsArray,
+  questionStep,
+  setQuestionStep,
+}) {
+  const [answer, setAnswer] = useState();
+  const { score, setScore } = useScore();
+  const router = useRouter();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setQuestionStep((prev) => prev + 1);
+    setScore((prev) => prev + answer);
+    console.log(answer);
+  }
+
+  const endOfQuestionnaire = questionStep === questionsArray.length;
+  if (endOfQuestionnaire) {
+    router.push("/leaderboard");
+  }
+
   return (
-    <div>
-      <form>
-        <label>{question}</label>
-        {answers.map((answer, index) => {
-          <div>
-            <input type={"checkbox"} id={`answer-${index}`} />
-            <label for={`answer-${index}`}>{answer.title}</label>
-          </div>;
-        })}
-      </form>
+    <div className="container mx-auto p-4">
+      {!endOfQuestionnaire ? (
+        <form
+          className="w-full flex flex-col gap-4 items-center"
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          <label className="text-2xl md:w-1/2 text-center">
+            {questionsArray[questionStep].question}
+          </label>
+          {questionsArray[questionStep].answers.map((answer, index) => {
+            return (
+              <div
+                key={answer.options}
+                className="rounded-md border-2 p-2 flex gap-2 w-full border-blue-700 md:max-w-[600px]"
+              >
+                <input
+                  value={answer}
+                  onChange={() => setAnswer(answer.points)}
+                  name={"options"}
+                  type={"radio"}
+                  id={`answers${index}`}
+                  required
+                />
+                <label
+                  className="w-full"
+                  value={answer.points}
+                  htmlFor={`answers${index}`}
+                >
+                  {answer.options}
+                </label>
+              </div>
+            );
+          })}
+          <button className="rounded bg-blue-700 w-1/2 p-2 text-white text-lg">
+            Next
+          </button>
+        </form>
+      ) : // <div>
+      //   <h1>Questionnaire finished</h1>
+      //   <Link href="/leaderboard">
+      //     <button>View Score</button>
+      //   </Link>
+      // </div>
+      null}
     </div>
   );
 }
