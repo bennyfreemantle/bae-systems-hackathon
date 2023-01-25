96c64a94687d4ff6f6910ae9225d2544c74fb457
bennyfreemantle <bennyfreemantle@protonmail.com>
2023-01-25 12:56:43 +0000

    basic styling to tile components

--- components/TileContainer/Tile/Tile.js
+++ components/TileContainer/Tile/Tile.js
@@ -4,12 +4,14 @@ import React from "react";
 export default function Tile({ title, desc, buttonText, hrefLink, className }) {
   return (
     <div
-      className={`p-4 flex flex-col gap-2 border-2 rounded-md border-slate-900 h-36 md:w-80 ${className}`}
+      className={`p-4 flex flex-col gap-2 border-2 rounded-md border-slate-900 h-44 md:w-80 ${className}`}
     >
-      <h1>{title}</h1>
-      <p>{desc}</p>
+      <h1 className="text-xl text-center font-semibold">{title}</h1>
+      <p className="flex-1">{desc}</p>
       <Link href={hrefLink}>
-        <button>{buttonText}</button>
+        <button className="rounded bg-blue-700 p-2 text-white w-full hover:bg-blue-500">
+          {buttonText}
+        </button>
       </Link>
     </div>
   );
