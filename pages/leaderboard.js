c8ec345d70ff3a6e3a1e0700e7cdd406ef1b0728
luke123adams <lxa207@gmail.com>
2023-01-25 14:05:58 +0000

    added medals to leaderboard

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -6,15 +6,19 @@ export default function Leaderboard () {
 
 
     const {score} = useScore()
+    const medal = score <= 6 ? 'Bronze' : score > 6 && score <= 12 ? 'Silver' : 'Gold'
+
+
+    
 
     return (
         <div className="container mx-auto p-3 bg-red-400 flex flex-col items-center">
         <p>Leaderboard</p>
         <div>
-        <p>Your score: {score}</p>
-        <p>Mukti: 12</p>
-        <p>Luke: 7</p>
-        <p>Ben: 4</p>
+        <p>Your score: {score} {medal} </p>
+        <p>Mukti: 13 Gold</p>
+        <p>Luke: 7 Silver</p>
+        <p>Ben: 4 Bronze</p>
         </div>
         </div>
     )
